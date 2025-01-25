dce1ea30ac3ec693873ff06f00a6fe4ad6e105fb
MrBoxs <rizkywanda200155@gmail.com>
2025-01-25 20:51:16 +0700

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,53 @@
+import type { Metadata } from "next";
+
+import "@/styles/globals.css";
+
+import { geistMono, geistSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+export const metadata: Metadata = {
+  title: "mrboxs",
+  description: "a personal website",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: dark)",
+        href: "https://j85ducwzne.ufs.sh/f/3tEolv156eYjnjPhoMt3agoYM2VHKlrbieBQPO3F6XWLGRzu",
+        url: "https://j85ducwzne.ufs.sh/f/3tEolv156eYjnjPhoMt3agoYM2VHKlrbieBQPO3F6XWLGRzu",
+      },
+      {
+        media: "(prefers-color-scheme: light)",
+        href: "https://j85ducwzne.ufs.sh/f/3tEolv156eYjnjPhoMt3agoYM2VHKlrbieBQPO3F6XWLGRzu",
+        url: "https://j85ducwzne.ufs.sh/f/3tEolv156eYjnjPhoMt3agoYM2VHKlrbieBQPO3F6XWLGRzu",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning={true}>
+      <body
+        className={cn(
+          "font-geist-sans antialiased",
+          geistSans.variable,
+          geistMono.variable,
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
